a8a5897e6725b73598fba98b9850706210840a77
Roman-Ojha <razzroman98@gmail.com>
2022-06-21 20:29:32 +0545

    docs(tutorial): Introduction to react native

---
+++ Class/Class_01_Introduction/app.js
@@ -0,0 +1,38 @@
+/*
+ *) React native use for create Mobile App(IOS and Android)
+ *) React native use react library for app development
+ *) React native use Native UI component for make A native mobile app
+ *) You can Quick start with React-native if you are familiar with React js
+ *) React native render native widget according to platform
+ *) React native gives access to Native Platform APIs
+ *) React native connect Javascript to Native Code
+ *) React native Compile UI Components to Native Code
+ *) But React native not Compile Logic Like sending HTTP request
+ *) React Native Codes: ReactNative UI + Javascript Logic
+ *) Native APP: Native UI + Javascript Logic
+ *) React Native Javascript Code runs on special virtual machine
+ */
+
+/*
+    EX:
+        -> In React.js = <div>
+        -> In Android  = android.view
+        -> In IOS = UIView
+        -> In React Native = <view>
+ */
+
+/*
+ *) initial release of react native in 2015
+ *) React-native is developed by Facebook community
+ *) It save the cost and development time to build 2 platform application with same code
+ */
+
+/*
+# How to start:
+    1) Install java
+    2) Install Android Studio
+    3) Node and Npm
+    4) React-native library
+    5) react-native run-android :: react-native run-ios
+    6) React-native start
+*/
---
+++ Class/Class_02_install_and_environment_setup/app.js
@@ -0,0 +1,24 @@
+/*
+    -> https://reactnative.dev/docs/environment-setup
+
+    Step 1) install Node and JDK
+    Step 2) npm install -g react-native-cli (to install react-native globaly)
+    Step 3) install VSCode
+    Step 4) Install Android Studio
+    Step 5) Install Android SDK Pie
+    Step 6) add ANDROID_HOME to User Environmental Variable
+                -> Variable name: ANDROID_HOME
+                -> Variable Value: C:\Users\Razz Roman\AppData\Local\Android\Sdk
+    Step 7) Create New React Native Project
+            -> npm react-native init <Project_Name>
+            -> npx react-native init <Project_Name>
+    Step 8) put emulator in env : C:\Users\Razz Roman\AppData\Local\Android\Sdk\emulator
+            -> emulator -list-avds (to get the list of emulator)
+            -> emulator -avd <emulator_Name>
+    Step 9) Open project path in terminal & type:
+            -> npx react-native start (to Start Metro)
+            -> npx react-native run-android (to Start Application)
+            -> npx react-native run-ios (to Start Application)
+
+    react-native start
+*/

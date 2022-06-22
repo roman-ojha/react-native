011552670ae107fb64ae102afca41fe66ac3662b
Roman-Ojha <razzroman98@gmail.com>
2022-06-22 22:50:47 +0545

    docs(tutorial): React Navigation Setup

---
+++ Class/Topic_Wise/19_React_Navigation_Setup/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {globalStyles} from '../../../styles/global';
+
+const Home = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
---
+++ Class/Topic_Wise/19_React_Navigation_Setup/screens/ReviewDetail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {globalStyles} from '../../../styles/global';
+
+const ReviewDetail = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text>ReviewDetail Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ReviewDetail;

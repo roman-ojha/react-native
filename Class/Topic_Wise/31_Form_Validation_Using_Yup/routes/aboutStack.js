24c13359d056a5fb61b6430b6bb6a2f1717db290
Roman-Ojha <razzroman98@gmail.com>
2022-06-23 20:06:54 +0545

    docs(tutorial): show form error while validating form

---
+++ Class/Topic_Wise/31_Form_Validation_Using_Yup/routes/aboutStack.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import About from '../screens/About';
+import Header from '../shared/Header';
+
+// This is the Stack Navigation for About Stack
+const HomeStack = createStackNavigator();
+function MyAboutStack() {
+  return (
+    <HomeStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#eee',
+          height: 60,
+        },
+        headerTintColor: '#444',
+      }}>
+      <HomeStack.Screen
+        name="About"
+        component={About}
+        options={({navigation}) => {
+          return {
+            headerTitle: () => (
+              <Header navigation={navigation} title="About GameZone" />
+            ),
+          };
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+}
+
+export default MyAboutStack;
---
+++ Class/Topic_Wise/31_Form_Validation_Using_Yup/shared/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {Dimensions} from 'react-native';
+const ScreenWidth = Dimensions.get('window').width;
+
+const Header = ({navigation, title}) => {
+  const openMenu = () => {
+    navigation.openDrawer();
+  };
+
+  return (
+    <ImageBackground
+      style={styles.header}
+      source={require('../../../assets/game_bg.png')}>
+      <MaterialIcons
+        name="menu"
+        color="#333"
+        size={28}
+        onPress={openMenu}
+        style={styles.icon}
+      />
+      <View style={styles.headerTitle}>
+        <Image
+          style={styles.headerImage}
+          source={require('../../../assets/heart_logo.png')}
+        />
+        <Text style={styles.headerText}>{title}</Text>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: ScreenWidth,
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#333',
+    letterSpacing: 1,
+  },
+  icon: {
+    position: 'absolute',
+    left: 16,
+  },
+  headerTitle: {
+    flexDirection: 'row',
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10,
+  },
+});
+
+export default Header;

22280040f592d3c349b95e221d0e6a9860ac278e
Roman-Ojha <razzroman98@gmail.com>
2022-07-18 13:31:58 +0545

    build(practice): initilized expo

---
+++ Practice/expo-cli/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.tsx start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+  },
+});
